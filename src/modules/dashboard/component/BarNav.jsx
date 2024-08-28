8147d256e348bd7eb0056cf1a519382c60c6e00e
KevinShcmidt <kevinrakoto77@gmail.com>
2024-08-28 16:15:46 +0300

    feat:ajout_pop_de_notification

--- src/modules/dashboard/component/BarNav.jsx
+++ src/modules/dashboard/component/BarNav.jsx
@@ -5,17 +5,26 @@ import { FiSearch } from "react-icons/fi";
 import UserIcon from "@/assets/userIcone.png"
 
 import { useContext, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import Notification from "./Notification";
 
 const BarNav = ({onSearch}) => {
   const { openNav } = useContext(Context);
   const [search, setSearch] = useState(false);
   const searchRef = useRef(null);
-  console.log(openNav);
+  const [notification, setNotification] = useState(false);
+  const notificationRef = useRef(null);
+  const handleNotifications = () => {
+    setNotification((prev) => !prev);
+  };
   
 
   const handleClickOutside = (event) => {
       if (searchRef.current && !searchRef.current.contains(event.target)) {
           setSearch(false);
+      }
+      if (notificationRef.current && !notificationRef.current.contains(event.target)) {
+        setNotification(false);
       }
   };
 
@@ -24,15 +33,11 @@ const BarNav = ({onSearch}) => {
   }
 
   useEffect(() => {
-      if (search) {
-          document.addEventListener("click", handleClickOutside);
-      } else {
-          document.removeEventListener("click", handleClickOutside);
-      }
-      return () => {
-          document.removeEventListener("click", handleClickOutside);
-      };
-  }, [search]);
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
   return (
     <div className="w-[100%] absolute top-0 py-[1.3vw] px-6">
       <div className="w-[100%] flex items-center justify-between  mx-auto h-[100%] text-white">
@@ -55,13 +60,15 @@ const BarNav = ({onSearch}) => {
           </div>
         </div>
         <div className="w-[50%] flex justify-end items-center gap-4 ">
-          <div className="relative text">
+          <div className="relative text" onClick={handleNotifications}
+            ref={notificationRef} >
             <BellAlertIcon className="w-[1.5rem]  h-[1.5rem] cursor-pointer text-[#969292]" />
             <span
               className=" absolute bg-[#F2505D]  h-[1rem] w-[1rem] text-center rounded-full -top-2 -right-1 font-semibold flex items-center justify-center  text-[13px]"
             >
               6
             </span>
+            {notification && <Notification isVisible={notification} />}
           </div>
           <div className="w-[1.8rem] h-[1.8rem] cursor-pointer border-2 border-[#D9D9D9] rounded-[50%]">
             <img className="w-full h-full" src={UserIcon} alt="" />
