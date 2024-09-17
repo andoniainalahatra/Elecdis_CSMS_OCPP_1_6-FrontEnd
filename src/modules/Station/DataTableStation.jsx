db2cfc5054963bebb79cc556f0cf37d858fbb9c4
TahianaRanaivoarilala <didiranaivoarilala@gmail.com>
2024-09-17 13:49:05 +0300

    feat:Integration-Api

--- src/modules/Station/DataTableStation.jsx
+++ src/modules/Station/DataTableStation.jsx
@@ -3,14 +3,15 @@ import DataTable from "@/components/Privates/forms/tables/DataTable";
 import stationData from "./data";
 import Columns from "@/components/Privates/forms/tables/Columns";
 import ButtonAction from "@/components/Privates/forms/tables/ButtonAction";
+import Example from "./data";
 
 
-const datas = ["id", "name", "location", "status", "power", "connector1", "connector2", "lastCommunication", "Actions"];
+const datas = ["id", "adresse", "Actions"];
 const columns = Columns(datas);
 const actions = [{ name: "detail" }, { name: "edit" }, { name: "delete" }]
 const DataTableStation = () => {
   return (
-    <DataTable columns={columns} datas={stationData} actions={actions} ButtonAction={ButtonAction} />
+    <DataTable columns={columns} datas={Example} actions={actions} ButtonAction={ButtonAction} />
 
   );
 }
