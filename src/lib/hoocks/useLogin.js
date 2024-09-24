0696a3b1d26b3e8c679f762649bdf6e82ff59e14
Randy <randyjoseph0000@gmail.com>
2024-09-24 15:55:50 +0300

    commit

--- src/lib/hoocks/useLogin.js
+++ src/lib/hoocks/useLogin.js
@@ -1,6 +1,6 @@
-import { loginUser } from '@/features/auth/authApi';
-import { useMutation } from '@tanstack/react-query';
-import Cookies from 'js-cookie';
+import { loginUser } from "@/features/auth/authApi";
+import { useMutation } from "@tanstack/react-query";
+import Cookies from "js-cookie";
 
 export const useLogin = () => {
   return useMutation({
@@ -8,19 +8,19 @@ export const useLogin = () => {
     onSuccess: (data) => {
       // const tokenExpiry = data.rememberMe ? 7 : 1;
       const tokenExpiry = 1; // 7 jours si "rememberMe" est activé, sinon 1 jour
-      Cookies.set('access_token', data.access_token, {
+      Cookies.set("access_token", data.access_token, {
         expires: tokenExpiry,
         secure: true, // Obligatoire si SameSite=None
-        sameSite: 'None' // Ou 'Lax', selon le comportement souhaité
+        sameSite: "None", // Ou 'Lax', selon le comportement souhaité
       });
-      Cookies.set('user', JSON.stringify(data.user), {
+      Cookies.set("user", JSON.stringify(data.user), {
         expires: tokenExpiry,
         secure: true,
-        sameSite: 'None'
+        sameSite: "None",
       });
     },
     onError: (error) => {
-      console.error('Login failed:', error);
+      console.error("Login failed:", error);
     },
   });
 };
