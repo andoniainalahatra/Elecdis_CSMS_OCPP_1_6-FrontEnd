3e6b88ea1d1f8de9a4df57503f2fed043e44f576
KevinShcmidt <kevinrakoto77@gmail.com>
2024-10-22 13:43:20 +0300

    feat:design_template_partenaire_contrats

--- src/modules/dashboard/content/PARTENAIRES/Contrats.jsx
+++ src/modules/dashboard/content/PARTENAIRES/Contrats.jsx
@@ -1,7 +1,15 @@
-const Contrats = () => {
-    return (
-        <div>Contrats</div>
-    )
-}
+// pages/Contracts.js
+import React from 'react';
+import ContractsTable from './components/ContractsTable';
+import AddContractForm from './components/AddContractForm';
 
-export default Contrats
+const Contrats = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <AddContractForm />
+      <ContractsTable />
+    </div>
+  );
+};
+
+export default Contrats;
