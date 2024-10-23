90817293806e711600b6fdb1f3ca75b9c307a18d
KevinShcmidt <kevinrakoto77@gmail.com>
2024-10-23 14:31:04 +0300

    fix:title_of_each_section

--- src/modules/dashboard/content/ACTIFS/CpNotices.jsx
+++ src/modules/dashboard/content/ACTIFS/CpNotices.jsx
@@ -3,7 +3,7 @@ import ChargePointNotices from "@/modules/Station/ChargePointNotices.jsx";
 const CpNotices = () => {
     return (
         <div className="p-6">
-            <div className="">CpNotices</div>
+            <div className="text-[24px] text-[#212B36]">Charge Point Notification</div>
             <ChargePointNotices/>
         </div>
     )
