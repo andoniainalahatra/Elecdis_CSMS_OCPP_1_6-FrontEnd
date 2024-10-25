d66ea2f4694359c5eb324c4fd89e6dd4f01f7dd4
KevinShcmidt <kevinrakoto77@gmail.com>
2024-10-25 16:21:55 +0300

    feat:formatage_prix_energy_consomme

--- src/lib/hoocks/useGetDataNoParams.js
+++ src/lib/hoocks/useGetDataNoParams.js
@@ -3,10 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 const useGetDataNoParams = (url, querykey) => {
   return useQuery({
     queryKey: [querykey],
-    queryFn: () =>
-      axiosInstance
-        .get(url)
-        .then((response) => response.data),
+    queryFn: () => axiosInstance.get(url).then((response) => response.data),
     refetchOnWindowFocus: true,
   });
 };
