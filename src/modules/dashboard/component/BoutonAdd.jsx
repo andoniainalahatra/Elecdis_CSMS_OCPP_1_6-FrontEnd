dd60561599ed814bb870bafce23ae958ef7d3fbc
TahianaRanaivoarilala <didiranaivoarilala@gmail.com>
2024-12-03 09:29:14 +0300

    fix:couleur

--- src/modules/dashboard/component/BoutonAdd.jsx
+++ src/modules/dashboard/component/BoutonAdd.jsx
@@ -5,8 +5,8 @@ const BoutonAdd = ({ open, Composant, setOpen }) => {
     return (
         <div className="">
             <button onClick={() => setOpen(n => !n)}
-                className='text-white  h-[45px] bg-blue-700 shadow-md shadow-blue-200 hover:bg-blue-600 hover:shadow-sm hover:shadow-blue-400 p-5 flex items-center font-semibold max-md:text-sm test-[14px]  rounded-full space-x-2 transition duration-300 ease-in-out'>
-                <FaPlus className='w-[0.6rem] h-[0.6rem]' />
+                className='text-white  h-[45px] bg-blue-700 shadow-md shadow-blue-200 hover:bg-blue-800 hover:shadow-sm hover:shadow-blue-400 p-5 flex items-center font-semibold max-md:text-sm test-[14px]  rounded-full space-x-2 transition duration-300 ease-in-out'>
+                <FaPlus className='w-[1rem] h-[1rem]' />
 
                 <span>Ajouter nouveau</span>
             </button>
